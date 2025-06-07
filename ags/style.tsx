d9b034ef9448203aa8821093435cc4224632ba18
Ivan Paraskun <ig.paraskun@gmail.com>
2025-06-07 22:55:29 +0700

    env

--- ags/style.tsx
+++ ags/style.tsx
@@ -2,16 +2,16 @@ import { App } from "astal/gtk4"
 import { exec } from "astal"
 
 export function ToggleMocha() {
-  exec("/home/paraskun/.config/ags/script/mocha.sh")
+  exec("/home/paraskun/paraskun/script/mocha.sh")
   App.apply_css("/tmp/mocha.css")
 }
 
 export function ToggleFrappe() {
-  exec("/home/paraskun/.config/ags/script/frappe.sh")
+  exec("/home/paraskun/paraskun/script/frappe.sh")
   App.apply_css("/tmp/frappe.css")
 }
 
 export function ToggleLatte() {
-  exec("/home/paraskun/.config/ags/script/latte.sh")
+  exec("/home/paraskun/paraskun/script/latte.sh")
   App.apply_css("/tmp/latte.css")
 }
