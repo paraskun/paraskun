90a286838a807b1855a3254f3030333aa8e5af3e
paraskun <ig.paraskun@gmail.com>
2026-03-12 21:57:25 +0700

    :)

--- setup/dc-u24.04.sh
+++ setup/dc-u24.04.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+touch $HOME/.bash_profile
+
 sudo apt update && sudo apt install -y  \
   stow                                  \
   wget                                  \
   tar                                   \
   ripgrep                               \
-  nodejs                                \
   npm
 
 if [[ ! -d "/opt/nvim-linux-x86_64" ]]; then
   wget https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-linux-x86_64.tar.gz
   sudo tar -xzf nvim-linux-x86_64.tar.gz -C /opt
   rm nvim-linux-x86_64.tar.gz
-  echo "export PATH=/opt/nvim-linux-x86_64/bin:$PATH" >> $HOME/.bashrc
+  echo "export PATH=/opt/nvim-linux-x86_64/bin:$PATH" >> $HOME/.bash_profile
 fi
 
 sudo npm install -g tree-sitter-cli
-
-cd $HOME/paraskun && stow . && cd
+stow tmux nvim
  
 /opt/nvim-linux-x86_64/bin/nvim --headless "+Lazy! install" +qa
 /opt/nvim-linux-x86_64/bin/nvim --headless "+TSUpdate" +qa
---
+++ setup/ws-f43.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+touch $HOME/.bash_profile
+
+sudo dnf install -y \
+  stow              \
+  wget              \
+  tar               \
+  ripgrep           \
+  swww              \
+  npm
+
+if [[ ! -d "/opt/nvim-linux-x86_64" ]]; then
+  wget https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-linux-x86_64.tar.gz
+  sudo tar -xzf nvim-linux-x86_64.tar.gz -C /opt
+  rm nvim-linux-x86_64.tar.gz
+  echo "export PATH=/opt/nvim-linux-x86_64/bin:$PATH" >> $HOME/.bash_profile
+fi
+
+sudo npm install -g tree-sitter-cli
+stow tmux nvim hypr ghostty walls
+ 
+/opt/nvim-linux-x86_64/bin/nvim --headless "+Lazy! install" +qa
+/opt/nvim-linux-x86_64/bin/nvim --headless "+TSUpdate" +qa
+
