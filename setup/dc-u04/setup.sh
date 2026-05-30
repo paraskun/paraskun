540141746244c96d65e57be7bcc5133ef14ac45d
Ivan Paraskun <ig.paraskun@gmail.com>
2026-05-30 13:14:12 +0700

    setup: fix

--- setup/dc-u04/setup.sh
+++ setup/dc-u04/setup.sh
@@ -5,5 +5,7 @@ stow nvim
 
 . setup/tools/nvim.sh
 
-cat setup/dc-u04/.profile >> $HOME/.bash_profile
+cat sh/.bash_profile >> $HOME/.bash_profile
+cat sh/.bashrc >> $HOME/.bashrc
+cat setup/dc-u04/.profile >> $HOME/.bashrc
 
