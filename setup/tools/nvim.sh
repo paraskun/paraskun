02be7eef5f1029fdeebfae090222f060d08c26a1
Ivan Paraskun <ig.paraskun@gmail.com>
2026-06-12 00:15:53 +0700

    nvim: fix setup

--- setup/tools/nvim.sh
+++ setup/tools/nvim.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+VERSION="0.12.2"
 ARCH=$(uname -m)
+
+case "${ARCH}" in
+"x86_64")
+  ARCH="amd64"
+  ;;
+"arm64" | "aarch64")
+  ARCH="arm64"
+  ;;
+*)
+  echo "Unsupported architecture."
+  exit 1
+  ;;
+esac
 
 ### nvim installation
 
 if [[ ! -d "/opt/nvim-linux-${ARCH}" ]]; then
-  wget https://github.com/neovim/neovim/releases/download/v0.12.2/nvim-linux-${ARCH}.tar.gz
+  wget https://github.com/neovim/neovim/releases/download/v${VERSION}/nvim-linux-${ARCH}.tar.gz
   sudo tar -xzf nvim-linux-${ARCH}.tar.gz -C /opt
   sudo ln -sf /opt/nvim-linux-${ARCH}/bin/nvim /usr/local/bin/nvim
   rm nvim-linux-${ARCH}.tar.gz
@@ -30,4 +44,3 @@ fi
 
 nvim --headless "+Lazy! install" +qa
 nvim --headless "+TSUpdate" +qa
-
